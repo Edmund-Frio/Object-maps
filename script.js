c4794bb9aefac9abb9a71b250be335873aaa4475
Edmund Frio <edmundfrio@gmail.com>
2023-10-06 13:03:45 -1000

    Create script.js

---
+++ script.js
@@ -0,0 +1,26 @@
+let courses = {
+   "170": {
+      title: "Introduction to Programming",
+      description: "Develop algorithms",
+      creditHours: 5, 
+   },
+   "250": {
+      title: "Web Development",
+      description: "Build web applications",
+      creditHours: 3,
+   },
+   "310": {
+      title: "Operating Systems",
+      description: "Process management and memory management",
+      creditHours: 3,
+   },
+   "430": {
+      title: "Artificial Intelligence",
+      description: "Simulate human thinking",
+      creditHours: 2,
+   }
+};
+
+for (let courseNum in courses) {
+   console.log(courseNum + " - " + courses[courseNum].title + ", " + courses[courseNum].description + ", " + courses[courseNum].creditHours);
+}
